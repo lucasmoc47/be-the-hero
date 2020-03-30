91a283050caa8fb43ce2f0eb83011b20423bd9dc
Lucas <lucasmoc47@gmail.com>
2020-03-29 22:33:00 -0300

    Finishing mobile app

--- mobile/src/pages/Incidents/index.js
+++ mobile/src/pages/Incidents/index.js
@@ -12,6 +12,8 @@ import logoImg from '../../assets/logo.png'
 export default function Incidents() {
 	const [incidents, setIncidents] = useState([])
 	const [total, setTotal] = useState([])
+	const [page, setPage] = useState(1)
+	const [loading, setLoading] = useState(false)
 
 	const navigation = useNavigation()
 
@@ -20,10 +22,20 @@ export default function Incidents() {
 	}
 
 	async function loadIncidents() {
-		const response = await api.get('incidents')
+		if(loading) return
 
-		setIncidents(response.data)
+		if(total > 0 && incidents.length === total) return 
+
+		setLoading(true)
+
+		const response = await api.get('incidents', {
+			params: { page }
+		})
+
+		setIncidents([...incidents, ...response.data])
 		setTotal(response.headers['x-total-count'])
+		setPage(page + 1)
+		setLoading(false)
 	}
 
 	useEffect(() => {
@@ -47,6 +59,8 @@ export default function Incidents() {
 				data={incidents}
 				keyExtractor={incident => String(incident.id)}
 				showsVerticalScrollIndicator={false}
+				onEndReached={loadIncidents}
+				onEndReachedThreshold={0.2}
 				renderItem={({ item: incident }) => (
 					<View style={styles.incident}>
 						<Text style={styles.incidentProperty}>ONG:</Text>
